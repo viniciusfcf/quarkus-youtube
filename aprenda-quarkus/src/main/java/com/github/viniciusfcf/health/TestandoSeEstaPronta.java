cf8b01d4154d767807203eeb41af384a0d183d04
Vinicius Ferraz <vinicius.ferraz@gmail.com>
2019-09-27 00:40:27 -0300

    corrigindo encoding e tirando import desnecessario

--- aprenda-quarkus/src/main/java/com/github/viniciusfcf/health/TestandoSeEstaPronta.java
+++ aprenda-quarkus/src/main/java/com/github/viniciusfcf/health/TestandoSeEstaPronta.java
@@ -4,7 +4,6 @@ import javax.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.Liveness;
 import org.eclipse.microprofile.health.Readiness;
 
 @Readiness
