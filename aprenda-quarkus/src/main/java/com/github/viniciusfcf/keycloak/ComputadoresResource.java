32ff628cfd82c63df941f0cdc9d817e02fb8fdaa
Vinicius Ferraz <vinicius.ferraz@gmail.com>
2019-10-12 02:13:17 -0300

    Video 6. Integracao com keycloak

---
+++ aprenda-quarkus/src/main/java/com/github/viniciusfcf/keycloak/ComputadoresResource.java
@@ -0,0 +1,39 @@
+package com.github.viniciusfcf.keycloak;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.keycloak.KeycloakSecurityContext;
+
+@Path("/computadores")
+@SecurityScheme(securitySchemeName = "quarkus-oauth", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/quarkus/protocol/openid-connect/token")))
+public class ComputadoresResource {
+
+    @Inject
+    KeycloakSecurityContext context;
+
+    @GET
+    @Path("publico")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String methodname() {
+        return "hello publico";
+    }
+
+    @GET
+    @Path("protegido")
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed("user")
+    @SecurityRequirement(name = "quarkus-oauth")
+    public String protegido() {
+        return "hello protegido "+context.getToken().getPreferredUsername();
+    }
+}
