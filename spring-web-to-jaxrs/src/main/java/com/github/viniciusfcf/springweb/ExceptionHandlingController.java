049a9c80ef7f52539b22d2c263fa7f2c9a51f4f8
Vinicius <vinicius.ferraz@gmail.com>
2020-05-11 01:15:06 -0300

    sprinb-web migrado para JAX-RS

--- spring-web-to-jaxrs/src/main/java/com/github/viniciusfcf/springweb/ExceptionHandlingController.java
+++ spring-web-to-jaxrs/src/main/java/com/github/viniciusfcf/springweb/ExceptionHandlingController.java
@@ -1,19 +1,25 @@
 package com.github.viniciusfcf.springweb;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
-@RestControllerAdvice
-public class ExceptionHandlingController {
+// @RestControllerAdvice
+@Provider
+public class ExceptionHandlingController  implements ExceptionMapper<IllegalArgumentException>{
 
-  @ResponseStatus(value=HttpStatus.BAD_GATEWAY,
-                  reason="Data integrity violation")
+	@Override
+	public Response toResponse(IllegalArgumentException exception) {
+		return Response.status(Status.BAD_GATEWAY).entity("exemplo de mensagem").build();
+	}
+
+  // @ResponseStatus(value=HttpStatus.BAD_GATEWAY,
+  //                 reason="Data integrity violation")
   
-  @ExceptionHandler({IllegalArgumentException.class})
-  public String databaseError() {
-    return "exemplo de mensagem";
-  }
+  // @ExceptionHandler({IllegalArgumentException.class})
+  // public String databaseError() {
+  //   return "exemplo de mensagem";
+  // }
 
 }
--- spring-web-to-jaxrs/src/main/java/com/github/viniciusfcf/springweb/ProdutoController.java
+++ spring-web-to-jaxrs/src/main/java/com/github/viniciusfcf/springweb/ProdutoController.java
@@ -6,22 +6,25 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
-@RestController
-@RequestMapping(path="/produtos", consumes="application/json", produces = "application/json")
+import org.apache.commons.lang3.StringUtils;
+
+// @RestController
+// @RequestMapping(path="/produtos", consumes="application/json", produces = "application/json")
+@Path("produtos")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ProdutoController {
 
 
@@ -40,13 +43,22 @@ public class ProdutoController {
     }
 
 
-	@GetMapping("/{id}")
-    public Produto getById(@PathVariable(name = "id") Long id) {
+    // @GetMapping("/{id}")
+    @GET
+    @Path("/{id}")
+    public Produto getById(
+        // @PathVariable(name = "id") 
+        @PathParam("id")
+        Long id) {
         return produtos.get(id);
     }
 
-    @GetMapping
-    public List<Produto> buscarProdutos(@RequestParam(name = "nome") String nome) {
+    // @GetMapping
+    @GET
+    public List<Produto> buscarProdutos(
+        // @RequestParam(name = "nome") 
+        @QueryParam("nome")
+        String nome) {
         if(StringUtils.isBlank(nome)) {
             return new ArrayList<Produto>(produtos.values());
 
@@ -54,33 +66,53 @@ public class ProdutoController {
         return new ArrayList<Produto>(produtos.values().stream().filter(s -> s.getNome().equals(nome)).collect(Collectors.toList()));
     }
 
-    @PostMapping
-    public void addProduto(@RequestBody Produto produto) {
+    // @PostMapping
+    @POST
+    public void addProduto(
+        // @RequestBody 
+        Produto produto) {
         adicionarProduto(produto);
     }
 
-    @PutMapping("/{id}")
-    public void atualizarProduto(@PathVariable(name = "id") Long id, Produto produto) {
+    // @PutMapping("/{id}")
+    @PUT
+    @Path("/{id}")
+    public void atualizarProduto(
+        // @PathVariable(name = "id") 
+        @PathParam("id")
+        Long id, Produto produto) {
         produto.setId(id);
         adicionarProduto(produto);
 
     }
 
-    @PatchMapping("/{id}")
-    public void atualizarProduto2(@PathVariable(name = "id") Long id, Produto produto) {
+    // @PatchMapping("/{id}")
+    @PATCH
+    @Path("/{id}")
+    public void atualizarProduto2(
+        // @PathVariable(name = "id") 
+        @PathParam("id")
+        Long id, Produto produto) {
         produto.setId(id);
         adicionarProduto(produto);
     }
 
 
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable(name = "id") Long id) {
+    // @DeleteMapping("/{id}")
+    @DELETE
+    @Path("/{id}")
+    public void deleteById(
+        // @PathVariable(name = "id") 
+        @PathParam("id")
+        Long id) {
         produtos.remove(id);
     }
 
 
-    @GetMapping("/excecao")
-    public ResponseEntity<?> excecao() {
+    // @GetMapping("/excecao")
+    @GET
+    @Path("/excecao")
+    public void excecao() {
         throw new IllegalArgumentException();
     }
 
