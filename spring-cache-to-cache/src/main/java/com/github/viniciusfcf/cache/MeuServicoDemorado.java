24b9ee985b5ec9c56c074486bb41d8ca1c536711
Vinicius <vinicius.ferraz@gmail.com>
2020-06-15 23:56:48 -0300

    migrado spring cache para cache

--- spring-cache-to-cache/src/main/java/com/github/viniciusfcf/cache/MeuServicoDemorado.java
+++ spring-cache-to-cache/src/main/java/com/github/viniciusfcf/cache/MeuServicoDemorado.java
@@ -4,36 +4,48 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.enterprise.context.ApplicationScoped;
 
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
+import io.quarkus.cache.CacheInvalidate;
+import io.quarkus.cache.CacheInvalidateAll;
+import io.quarkus.cache.CacheKey;
+import io.quarkus.cache.CacheResult;
 
 @ApplicationScoped
 public class MeuServicoDemorado {
     
     private AtomicInteger inteiro = new AtomicInteger();
 
-/*
-    NAO SUPORTADO AINDA:
-    "key", "keyGenerator", "cacheManager", "cacheResolver", "condition", "unless", "sync", "beforeInvocation"
-*/
+    /*
+        Anotações suportadas:
+            @Cacheable
+            @CachePut
+            @CacheEvict
 
-    @Cacheable(value = "nome-do-cache")
+        NAO SUPORTADO AINDA:
+        "key", "keyGenerator", "cacheManager", "cacheResolver", "condition", "unless", "sync", "beforeInvocation"
+    */
+
+
+
+    // @Cacheable(value = "nome-do-cache")
+    @CacheResult(cacheName = "nome-do-cache")
     public String primeiroCacheavel(String id, Object entidade) {
         demore();
         return "Primeiro Método Cacheavel "+inteiro.getAndIncrement()+", ID: "+id;
     }
     
-    @CacheEvict(value = "nome-do-cache", allEntries = true)
+    // @CacheEvict(value = "nome-do-cache", allEntries = true)
+    @CacheInvalidateAll(cacheName = "nome-do-cache")
     public void limpePrimeiroCacheTodo() {
     }
 
-    @CacheEvict(value = "nome-do-cache")
+    // @CacheEvict(value = "nome-do-cache")
+    @CacheInvalidate(cacheName = "nome-do-cache")
     public void limpePrimeiroCache(String id, Object entidade) {
     }
 
 
-    @CachePut(value = "nome-do-cache")
+    // @CachePut(value = "nome-do-cache")
+    //Não temos uma anotação similar
     public String segundoCacheavel(String id, Object entidade) {
         demore();
         return "Segundo Cacheavel "+inteiro.getAndIncrement()+", ID: "+id;
